fd01f96b05859ee3617d9e337580effe6de25495
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-01-15 21:53:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/0fbec4b1d2bde372f927b.jpg https://telegra.ph/file/43a1e68be42930d9d4b60.jpg https://telegra.ph/file/7671db3e099f51ea0623a.jpg https://telegra.ph/file/0dd77540d95ff5edda3bf.jpg https://telegra.ph/file/eadcdffec0ae7472519c7.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
